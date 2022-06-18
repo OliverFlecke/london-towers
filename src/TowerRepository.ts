96bee540c1b7b57e81898d61641c42f843333ab8
Oliver Fleckenstein <oliverfl@live.dk>
2022-06-18 16:38:11 +0100

    feat: Reading database config from environment

--- src/TowerRepository.ts
+++ src/TowerRepository.ts
@@ -9,7 +9,12 @@ class TowerRepository {
   }
 
   public static async create(): Promise<TowerRepository> {
-    const uri = "mongodb://localhost:27017";
+    const host = process.env.DB_HOST ?? "localhost";
+    const port = process.env.DB_PORT ?? 27017;
+    const db = process.env.DB_NAME;
+
+    const uri = `mongodb://${host}:${port}/${db}`;
+    console.log(uri);
     const client = new MongoClient(uri);
     await client.connect();
     console.info("DB connected");
@@ -18,18 +23,11 @@ class TowerRepository {
   }
 
   public getTowers(): Promise<WithId<Tower>[]> {
-    return this.client
-      .db("london")
-      .collection<Tower>("tower")
-      .find({})
-      .toArray();
+    return this.client.db().collection<Tower>("tower").find({}).toArray();
   }
 
   public async addTowers(towers: Tower[]): Promise<void> {
-    await this.client
-      .db("london")
-      .collection<Tower>("tower")
-      .insertMany(towers);
+    await this.client.db().collection<Tower>("tower").insertMany(towers);
   }
 }
 
